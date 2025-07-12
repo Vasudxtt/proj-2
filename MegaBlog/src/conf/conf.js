ebdbff027052ca13b3433240c2e179ffeb6b918b
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-13 04:02:29 +0530

    config file so that the site dont crashes with console.log

---
+++ MegaBlog/src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+  appwriteUrl : Strin(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId : Strin(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId : Strin(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId : Strin(import.meta.env.VITE_APPWRITE_COLLECTION_IDL),
+  appwriteBucketId : Strin(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+
+export default conf
