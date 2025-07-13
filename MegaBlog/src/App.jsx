7e5c896fa4c3299d4cecd5ff7f28af12a38c9aef
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-14 00:19:26 +0530

    update

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -2,15 +2,24 @@ import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import authService from './appwrite/auth';
 import './App.css'
+import { login,logout } from './store/authSlice';
 
 function App() {
  const [loading, setLoading] = useState(true);
 
 
  useEffect(()=>{
-  authService,getCurrentUser()
-  .then((userData)=>{})
-  .finally()
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}));
+    } else{
+      dispatch(logout());
+    }
+  })
+  .finally(()=>{
+    setLoading(false);
+  })
  },[])
 
  const dispatch = useDispatch();
