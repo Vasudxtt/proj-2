2bd9a0c60b13277e7619187bd09cae22372fa405
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-14 00:29:50 +0530

    update

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,6 +1,6 @@
 const conf = {
-  appwriteUrl : Stringg(import.meta.env.VITE_APPWRITE_URL),
-  appwriteProjectId : Stringg(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_IDL),
   appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
