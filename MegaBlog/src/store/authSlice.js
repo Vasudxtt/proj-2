31ea3b8871a484aa9ff24b8e57c1a4b995e2aecb
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-14 00:04:06 +0530

    created auyh slice.js

--- MegaBlog/src/store/authSlice.js
+++ MegaBlog/src/store/authSlice.js
@@ -1,25 +1,25 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// // this is to track the authentication
+import { createSlice } from "@reduxjs/toolkit";
+// this is to track the authentication
 
-// const intialState = {
-//   status: false,
-//   user: null,
-// }
+const intialState = {
+  status: false,
+  user: null,
+}
 
-// const authSlice = createSlice({
-//   name: "auth",
-//   intialState,
-//   reducers:{
-//     login:(state,action)=>{
-//       state.status = true;
-//       state.userData = action.payload.userData;
-//     },
-//     logout:(state)=>{
-//       state.status = false;
-//       state.userData = null;
-//     }
-//   }
-// })
+const authSlice = createSlice({
+  name: "auth",
+  intialState,
+  reducers:{
+    login:(state,action)=>{
+      state.status = true;
+      state.userData = action.payload.userData;
+    },
+    logout:(state)=>{
+      state.status = false;
+      state.userData = null;
+    }
+  }
+})
 
-// export const {login,logout} = authSlice.actions;
-// export default authSlice.reducer;
+export const {login,logout} = authSlice.actions;
+export default authSlice.reducer;
