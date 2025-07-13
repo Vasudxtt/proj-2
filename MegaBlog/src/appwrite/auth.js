9372aca6ec5529575ab3c2aea60310a474722bd4
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-13 23:19:40 +0530

    update auth.js

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -1,7 +1,59 @@
 import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
 
-export class AuthService{}
+export class AuthService{
+  client = new client();
+  account;
+
+  constructor(){
+    this.client
+    .setEndpoint(conf.appwriteUrl)
+    .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({email,password,name}){
+    try {
+      const userAccount = await this.account.create(ID.unique(),email,password,name)
+
+      if (userAccount) {
+        // call another method
+         return this.login({email,password})
+      } else {
+        // throw error;
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+   async login({email,password}){
+    try {
+       return await this.account.createEmailPasswordSession(email,password);
+    } catch (error) {
+      throw error;
+    }
+   }
+
+   async getCurrentUser(){
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+
+  async logout(){
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+} 
 
 const authService = new AuthService();
 
