27c7c5cb02e4b6a89c3bd2c8f10251765ea8ee62
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-14 00:00:32 +0530

    made a config file

---
+++ MegaBlog/src/store/authSlice.js
@@ -0,0 +1,25 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// // this is to track the authentication
+
+// const intialState = {
+//   status: false,
+//   user: null,
+// }
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   intialState,
+//   reducers:{
+//     login:(state,action)=>{
+//       state.status = true;
+//       state.userData = action.payload.userData;
+//     },
+//     logout:(state)=>{
+//       state.status = false;
+//       state.userData = null;
+//     }
+//   }
+// })
+
+// export const {login,logout} = authSlice.actions;
+// export default authSlice.reducer;
---
+++ MegaBlog/src/store/store.js
@@ -0,0 +1,12 @@
+// import{configureStore} from "@reduxjs/toolkit"
+// // import { authSlice } from "./slices/authSlice";
+// // import { postSlice } from "./slices/postSlice";
+
+// const store = configureStore({
+//     reducer:{
+//         // auth: authSlice.reducer,
+//         // post: postSlice.reducer,
+//     }
+// });
+
+// export default store;
