860bcc8eb4e29552c7ec2b34c033d45e8ac8a661
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-17 00:46:04 +0530

    ww

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,7 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo/Logo";
 
 export default index.js= {
   Header,
-  Footer
+  Footer,
+  Container,
+  Logo
 }
