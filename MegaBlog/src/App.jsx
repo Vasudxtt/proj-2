65adb7b07211814b6477160e500eb7c2c71f1b8f
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-17 00:32:59 +0530

    fucked

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -3,8 +3,7 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
-import{Header} from "./components/Header/Header"
-import{Footer} from "./components/Footer/Footer"
+import index from "./components/index"
 import { Outlet } from 'react-router-dom'
 
 function App() {
