1a38d64112c51dc146291b9f47c0ae29090f8c9c
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-17 00:54:34 +0530

    a

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
-import Logo from "./Logo/Logo";
+import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 
 export default index.js= {
