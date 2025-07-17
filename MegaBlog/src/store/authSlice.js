f991a478d9122c78d63654dfe673b6e72cf75db2
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-17 17:13:40 +0530

    22

--- MegaBlog/src/store/authSlice.js
+++ MegaBlog/src/store/authSlice.js
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-// this is to track the authentication
 
-const intialState = {
-  status: false,
-  user: null,
+const initialState = {
+    status : false,
+    userData: null
 }
 
 const authSlice = createSlice({
-  name: "auth",
-  intialState,
-  reducers:{
-    login:(state,action)=>{
-      state.status = true;
-      state.userData = action.payload.userData;
-    },
-    logout:(state)=>{
-      state.status = false;
-      state.userData = null;
-    }
-  }
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+     }
 })
 
-export const {login,logout} = authSlice.actions;
+export const {login, logout} = authSlice.actions;
+
 export default authSlice.reducer;
